f156c08bc9e3a48aeef7e6df64d4ad2d48284c33
ADX <info@adigitalx.com>
2024-02-18 12:33:07 -0500

    cleaned up folder structure

---
+++ Webview_Mic-Cam_Permissions/iOS/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  transgraph
+//
+//  Created by David Espinoza on 1/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Display()
+            .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
